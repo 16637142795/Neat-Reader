e381d78db9896b03389e9edc7dea59b5b16b1eef
郭浩振 <52062893+16637142795@users.noreply.github.com>
2026-01-30 10:14:08 +0800

    打包

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 核心添加：匹配GitHub Pages仓库访问路径
+  base: '/Neat-Reader/',
   server: {
     host: '0.0.0.0',
     port: 8080,
